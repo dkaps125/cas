e6cea623d2a75ddd9e8fbf68272e67928cb13e1d
dkaps125 <dkapit@umd.edu>
2018-02-25 17:18:56 +0000

    Fixed cooperation with iris

--- middleware.go
+++ middleware.go
@@ -2,27 +2,35 @@ package cas
 
 import (
 	"net/http"
+    "fmt"
 
 	"github.com/golang/glog"
 )
 
-func (c *Client) Handler(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func (c *Client) Handler() func(http.ResponseWriter, *http.Request, http.HandlerFunc) {
+	return func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 		if glog.V(2) {
 			glog.Infof("cas: handling %v request for %v", r.Method, r.URL)
 		}
 
 		setClient(r, c)
+        c.getSession(w, r)
+
+        fmt.Printf("Handler called...\n")
 
 		if !IsAuthenticated(r) {
+            fmt.Printf("Redirecting to login...\n")
 			RedirectToLogin(w, r)
 			return
 		}
 
 		if r.URL.Path == "/logout" {
+            fmt.Printf("Redirecting to logout...\n")
 			RedirectToLogout(w, r)
 			return
 		}
-		h.ServeHTTP(w, r)
-	})
+
+        fmt.Printf("Handler exited...\n")
+        next(w, r)
+	}
 }
