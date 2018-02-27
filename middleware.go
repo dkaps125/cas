dab1aa9f666c18d09bbc12865f5b6c4909f789d8
dkaps125 <dkapit@umd.edu>
2018-02-27 00:24:21 +0000

    Removed UserData

--- middleware.go
+++ middleware.go
@@ -6,7 +6,7 @@ import (
 	"github.com/golang/glog"
 )
 
-func (c *Client) Handler() func(http.ResponseWriter, *http.Request, http.HandlerFunc) {
+func (c *Client) Handler(u *UserData) func(http.ResponseWriter, *http.Request, http.HandlerFunc) {
 	return func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 		if glog.V(2) {
 			glog.Infof("cas: handling %v request for %v", r.Method, r.URL)
